fc3d6d9ccd21514ff7721137864a9cdec1a04784
Oliver Johansson <46806877+Ojoh02@users.noreply.github.com>
2022-08-24 18:13:31 -0700

    tests

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,22 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(({ mode }) => {
-
-const env = loadEnv(
-  'mock', 
-  process.cwd(),
-  '' 
-)
-  const processEnvValues = {
-    'process.env': Object.entries(env).reduce(
-      (prev, [key, val]) => {
-        return {
-          ...prev,
-          [key]: val,
-        }
-      },
-      {},
-    )
-  }
-
-  return {
-    plugins: [vue()],
-    define: processEnvValues
-  }
+    const env = loadEnv(mode, process.cwd())
+  
+    const processEnvValues = {
+      'process.env': Object.entries(env).reduce(
+        (prev, [key, val]) => {
+          return {
+            ...prev,
+            [key]: val,
+          }
+        },
+        {},
+      )
+    }
+  
+    return {
+      plugins: [vue()],
+      define: processEnvValues
+    }
 })
